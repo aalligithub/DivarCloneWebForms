78b301c39d0e9eb7d362a20978a87b03f20a2725
Ali <83744482+aalligithub@users.noreply.github.com>
2024-11-28 01:56:53 +0330

    Moved utilities out into a new shared class library to prevent circular references and use utilities easier in the future

---
+++ Shared/Utilities.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class Logger
+    {
+        private static readonly Logger _instance = new Logger();
+
+        private Logger() { }
+
+        public static Logger Instance => _instance;
+
+        private static readonly string LogFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs/log.txt");
+
+        public void LogIntoFile(string message)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(LogFilePath));
+            File.AppendAllText(LogFilePath, $"{DateTime.Now}: {message}{Environment.NewLine}");
+        }
+
+        public void LogToOutput(string message) {
+            System.Console.WriteLine(message);
+        }
+
+        public void LogError(string message) => LogToOutput($"ERROR: {message}");
+        public void LogInfo(string message) => LogToOutput($"INFO: {message}");
+    }
+}
