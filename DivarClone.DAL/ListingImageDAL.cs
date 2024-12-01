0f03ea66265cbbcc087261a3abe531de9982b56f
Ali <83744482+aalligithub@users.noreply.github.com>
2024-12-02 02:04:58 +0330

    addeed a DAL for listing images but methods are pending

---
+++ DivarClone.DAL/ListingImageDAL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Shared;
+using static DivarClone.DAL.ListingDTO;
+
+namespace DivarClone.DAL
+{
+    public interface IListingImageDAL
+    {
+        List<ListingDTO> GetListingImagesFromDB();
+
+        void DeleteListingImageFromDB();
+
+        Task<bool> InsertImagePathIntoDB(int? ListingId, List<string> PathToImageFTP, string imageHash);
+    }
+    public class ListingImageDAL : IListingImageDAL
+    {
+
+    }
+}
